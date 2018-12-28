42184c83c670e391ed42cb8ed0dcbccba92d44a2
Qiuyun Chen <qiuyunchen@pursuit.org>
2018-12-28 15:38:09 -0500

    modified gif.js to reflect use of request module

--- services/gif.js
+++ services/gif.js
@@ -1,18 +1,37 @@
-const getGifUrls = (searchStr, cb) =>{
-    if (searchStr.trim() === '') return;
+// const XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
 
+// const getGifUrls = (searchStr, cb) =>{
+
+//     const apiKey = 'uDbAwYnCUT9uP4l50rUhKQCOaubt4SK8';
+//     const url = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${searchStr}`;
+
+//     const request = new XMLHttpRequest();
+//     request.open('GET', url);
+//     request.addEventListener('load', e =>{
+//         const obj = JSON.parse(e.currentResponse.data);
+//         const arr = [];
+//         obj.data.forEach( o => arr.push(o.images.original.url) );
+//         cb(arr);
+//     });
+//     request.send();
+// }
+
+
+const request = require('request');
+
+const getGifUrls = (searchStr, cb) =>{
     const apiKey = 'uDbAwYnCUT9uP4l50rUhKQCOaubt4SK8';
     const url = `https://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${searchStr}`;
 
-    const request = new XMLHttpRequest();
-    request.open('GET', url);
-    request.addEventListener('load', e =>{
-        const obj = JSON.parse(e.currentTarget.response);
+    request(url, (err, res, body) =>{
+        if(err) console.log("Error Code:", res.statusCode);
+
+        const obj = JSON.parse(body);
         const arr = [];
         obj.data.forEach( o => arr.push(o.images.original.url) );
         cb(arr);
     });
-    request.send();
 }
 
 module.exports = {getGifUrls};
+
